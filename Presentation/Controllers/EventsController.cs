07cfe9baa1212cba678979e722b66f8dfce1def9
cristinambdev <cristinamb.dev@gmail.com>
2025-05-13 11:35:34 +0200

    Add project files.

---
+++ Presentation/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Services;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController(IEventService eventService) : ControllerBase
+{
+    private readonly IEventService _eventService = eventService;
+}
---
+++ Presentation/Services/IEventService.cs
@@ -0,0 +1,6 @@
+﻿namespace Presentation.Services;
+
+public interface IEventService
+{
+
+}
